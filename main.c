15cfe9972726952e5018538edc8f9656a1bb5087
yg-cpu <anirudhyamunang@gmail.com>
2021-06-20 18:00:02 +0200

    fixed: blit display red

--- main.c
+++ main.c
@@ -15,8 +15,7 @@ int misc_rand()
 }
 
 int main(int argc, char *argv[])
-{
-	srand(time(NULL));
+{	srand(time(NULL));
 	ALLEGRO_DISPLAY* display         = NULL;
 	ALLEGRO_EVENT_QUEUE* event_queue = NULL;
 	ALLEGRO_TIMER* timer             = NULL;
@@ -122,11 +121,10 @@ int main(int argc, char *argv[])
 			enemy.y    = 1 + (rand() % al_get_display_width (display));
 			enemy.x    = 1 + (rand() % al_get_display_height(display));
 			
-			al_clear_to_color(al_map_rgba_f(255, 0, 0, 1));
 			switch (enemy.type)
 			{
 				case 1: // line
-					puts("Reached 1");
+					puts("line");
 					al_draw_line(enemy.x,enemy.y,
 						al_get_display_width(display),
 						al_get_display_height(display),
@@ -134,7 +132,7 @@ int main(int argc, char *argv[])
 						misc_rand());
 						al_flip_display(); break;
 				case 2: // triangle
-					puts("Reached 2");
+					puts("triangle");
 					al_draw_filled_triangle(enemy.x,
 						enemy.y,enemy.x,enemy.y,
 						al_get_display_width(display),
@@ -142,26 +140,26 @@ int main(int argc, char *argv[])
 						al_map_rgb(255,0,0));
 						al_flip_display(); break;
 				case 3: // rectangle
-					puts("Reached 3");
+					puts("rectangle");
 					al_draw_filled_rectangle(enemy.x,enemy.y,
 						al_get_display_width(display),
 						al_get_display_height(display),
 						al_map_rgb(255,0,0));
 						al_flip_display(); break;
 				case 4: // circle
-					puts("Reached 4");
+					puts("circle");
 					al_draw_filled_circle(enemy.x,enemy.y,
 						misc_rand(),al_map_rgb(225,0,0)); 
 						al_flip_display(); break;
 				case 5: // arc
-					puts("Reached 5");
+					puts("arc");
 					al_draw_arc(enemy.x,enemy.y,
 						misc_rand(),misc_rand(),
 						enemy.y,al_map_rgb(225,0,0),
 						misc_rand()); 
 						al_flip_display(); break;
 			}
-			enemy.redraw = false;
+//			enemy.redraw = false;
 		}
 
 		// Fetch the event (if one exists)
@@ -179,8 +177,6 @@ int main(int argc, char *argv[])
 					redraw = true;
 					break;
 				case ALLEGRO_EVENT_DISPLAY_CLOSE:
-					al_play_sample(gg,1.0f,0.0f,1.0f,
-							ALLEGRO_PLAYMODE_ONCE,0);
 					running = false;
 					break;
 				default:
@@ -214,6 +210,7 @@ int main(int argc, char *argv[])
 	al_uninstall_keyboard();
 	al_destroy_event_queue(event_queue);
 	al_destroy_sample(sfx);
-		
+	al_destroy_sample(gg);
+
 	return 0;
 }
